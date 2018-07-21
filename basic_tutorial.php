86cf2f6f4304230b85e578adffc3bf960ef6fba9
msc007 <alstjr5569@gmail.com>
2018-07-21 15:15:45 -0700

    first php

---
+++ basic_tutorial.php
@@ -0,0 +1,15 @@
+<?php
+    //print string
+    echo "Hello World";
+ 
+    //print out its data type & value
+    echo var_dump(1234);
+
+    //concatenation operator
+    echo "Hello"." "."World";
+    
+    //append string
+    $data1 = "Hello";
+    $data1 .= " World";
+
+?>
